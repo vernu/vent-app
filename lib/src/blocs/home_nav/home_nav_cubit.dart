cce9b8cbd61f96bff6c904d09c5310ff39db1706
isra el <vernu1997@gmail.com>
2021-12-05 10:14:45 +0300

    go got my account page if not signed in

--- lib/src/blocs/home_nav/home_nav_cubit.dart
+++ lib/src/blocs/home_nav/home_nav_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'home_nav_state.dart';
 
@@ -7,6 +8,12 @@ class HomeNavCubit extends Cubit<HomeNavState> {
   HomeNavCubit() : super(HomeNavState());
 
   void changeCurrentIndex(int index) {
+    emit(HomeNavState(selectedIndex: index));
+  }
+
+  void gotToSigninPageIfUnAuthenticated() {
+    FirebaseAuth _instance = FirebaseAuth.instance;
+    int index = _instance.currentUser != null ? 0 : 2;
     emit(HomeNavState(selectedIndex: index));
   }
 }
--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => HomeNavCubit()),
+        BlocProvider(create: (_) => HomeNavCubit()..gotToSigninPageIfUnAuthenticated()),
         BlocProvider(create: (_) => VentsBloc()..add(VentsLoadRequested())),
         BlocProvider(
             create: (_) =>
