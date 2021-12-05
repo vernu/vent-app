ce071ab4ff73385a99384a90dda8275d6aa987b2
isra el <vernu1997@gmail.com>
2021-12-05 10:31:00 +0300

    tab fix

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,7 +22,9 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
+    FirebaseAuth _instance = FirebaseAuth.instance;
+    int index = _instance.currentUser != null ? 0 : 2;
+    _pageController = PageController(initialPage: index);
   }
 
   @override
