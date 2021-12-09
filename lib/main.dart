93568c9ee7041f0a2ad8f47c1507cf0fd66b742b
isra el <vernu1997@gmail.com>
2021-12-09 16:30:13 +0300

    push notif

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:vent/src/blocs/auth/auth_bloc.dart';
 import 'package:vent/src/blocs/theme/theme_bloc.dart';
 import 'package:vent/src/router/app_router.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  description:
+      'This channel is used for important notifications.', // description
+  importance: Importance.high,
+);
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
   runApp(MyApp());
 }
 
@@ -15,39 +53,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.error.toString());
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MultiBlocProvider(
-              providers: [
-                BlocProvider(
-                  create: (_) => ThemeBloc()..add(ThemeInitial()),
-                ),
-                BlocProvider(
-                  create: (_) => AuthBloc(),
-                ),
-              ],
-              child: BlocBuilder<ThemeBloc, ThemeState>(
-                builder: (context, state) {
-                  return MaterialApp(
-                    title: 'Vent',
-                    // theme: ThemeData(
-                    //   primarySwatch: Colors.red,
-                    //   visualDensity: VisualDensity.adaptivePlatformDensity,
-                    // ),
-                    theme: state.themeData,
-                    onGenerateRoute: (routeSettings) =>
-                        AppRouter.onGenerateRoute(routeSettings),
-                  );
-                },
-              ),
-            );
-          }
-          return Center(child: CircularProgressIndicator());
-        });
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => ThemeBloc()..add(ThemeInitial()),
+        ),
+        BlocProvider(
+          create: (_) => AuthBloc(),
+        ),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'Vent',
+            // theme: ThemeData(
+            //   primarySwatch: Colors.red,
+            //   visualDensity: VisualDensity.adaptivePlatformDensity,
+            // ),
+            theme: state.themeData,
+            onGenerateRoute: (routeSettings) =>
+                AppRouter.onGenerateRoute(routeSettings),
+          );
+        },
+      ),
+    );
   }
 }
